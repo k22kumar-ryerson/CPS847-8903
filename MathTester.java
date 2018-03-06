bd0a22928e5456a6dc85d82d14659867ecf577a5
k22kumar-ryerson <36056519+k22kumar-ryerson@users.noreply.github.com>
2018-03-06 13:59:19 -0500

    Create MathTester.java

---
+++ MathTester.java
@@ -0,0 +1,23 @@
+package cps847midterm;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import cps847midterm.Math;
+
+
+public class Junit
+{
+    
+    public void testCps8903() {
+    
+        Math math = new Math();
+        
+        assertEquals("Test cps8903", "16", math.cps8903(4));
+    
+    }
+    
+    
+    
+    
+}
